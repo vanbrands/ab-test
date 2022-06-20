eeb2fd2bed3f35d6e2217bee01b589e27a646c6c
Felipe Brandão <36680823+vanbrands@users.noreply.github.com>
2022-06-20 16:53:58 +0000

    Add rudderstack script to head

---
+++ components/Hero.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Welcome to this website!</h1>
+        <button @click="trackClick">Click Here!!</button>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        trackClick(){
+            console.log('clicked')
+        }
+    }
+}
+</script>
---
+++ tracking/rudderstack/load-script.js
@@ -0,0 +1,28 @@
+export const analyticsScript = `
+    rudderanalytics = window.rudderanalytics = [];
+    var methods = [
+        "load",
+        "page",
+        "track",
+        "identify",
+        "alias",
+        "group",
+        "ready",
+        "reset",
+        "getAnonymousId",
+        "setAnonymousId",
+    ];
+    for (var i = 0; i < methods.length; i++) {
+    var method = methods[i];
+    rudderanalytics[method] = (function (methodName) {
+        return function () {
+        rudderanalytics.push(
+            [methodName].concat(Array.prototype.slice.call(arguments))
+        );
+        };
+    })(method);
+    }
+
+    rudderanalytics.load(<WRITE_KEY>, <DATA_PLANE_URL>);
+    rudderanalytics.page();
+`
